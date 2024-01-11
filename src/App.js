ee99f6904bd065ee81e80cd5e312a851b6caf1ab
Kiranv1410 <v.kiran707@gmail.com>
2024-01-12 00:36:30 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Nav from './Nav';
+import Cards from './cards'
+function App() {
+  return (
+    <>
+    <Nav/>
+    <Cards/>
+    </>
+  );
+}
+
+export default App;
